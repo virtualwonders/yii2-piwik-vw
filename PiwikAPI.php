de8d78c51f69b429ddf198f9fb24867624082804
Dave Ramos <dave@virtual-wonders.com>
2018-12-06 16:53:13 +0800

    Added PiwikAPI and PiwikSitesManager Class

---
+++ PiwikAPI.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace virtualwonders\piwikvw;
+
+
+use Yii;
+use yii\base\Component;
+use yii\base\Exception;
+
+/**
+ * Piwik API
+ * Lets the system talk to Piwik Analytics
+ */
+class PiwikAPI extends Component
+{
+    // Piwik error message on failed authentication
+    const TOKEN_FAILED = 'Token authentication failed, please contact administrator.';
+
+    // Referrers Method
+    const REFERRERS = 'Referrers';
+
+    const GET_REFERRER_TYPE  = '.getReferrerType';
+    const GET_ALL_REFERRER   = '.getAll';
+    const GET_SEARCH_ENGINES = '.getSearchEngines';
+    const GET_WEBSITES       = '.getWebsites';
+    const GET_CAMPAIGNS      = '.getCampaigns';
+    const GET_NUMBER_OF_DISTINCT_KEYWORDS = '.getNumberOfDistinctKeywords';
+    const GET_NUMBER_OF_DISTINCT_WEBSITES = '.getNumberOfDistinctWebsites';
+    const GET_NUMBER_OF_DISTINCT_CAMPAIGNS = '.getNumberOfDistinctCampaigns';
+    const GET_NUMBER_OF_DISTINCT_SEARCH_ENGINES = '.getNumberOfDistinctSearchEngines';
+
+    // Site Manager Method
+    const SITES_MANAGER = 'SitesManager';
+
+    const ADD_SITE         = '.addSite';
+    const UPDATE_SITE      = '.updateSite';
+    const DELETE_SITE      = '.deleteSite';
+    const GET_ALL_SITES    = '.getAllSites';
+    const GET_ALL_SITES_ID = '.getAllSitesId';
+    const GET_SITE_FROM_ID = '.getSiteFromId';
+    const GET_SITES_ID_FROM_SITE_URL = '.getSitesIdFromSiteUrl';
+    const GET_SITES_WITH_VIEW_ACCESS = '.getSitesWithViewAccess';
+    const GET_SITES_ID_WITH_VIEW_ACCESS = '.getSitesIdWithViewAccess';
+    const GET_SITES_ID_WITH_ATLEAST_VIEW_ACCESS = '.getSitesIdWithAtLeastViewAccess';
+
+    // User Manager Method
+    const UsersManager = 'UsersManager';
+
+    const ADD_USER          = '.addUser';
+    const UPDATE_USER       = '.updateUser';
+    const DELETE_USER       = '.deleteUser';
+    const USER_EXISTS       = '.userExists';
+    const USER_EMAIL_EXISTS = '.userEmailExists';
+    const GET_USER          = '.getUser';
+    const GET_USERS         = '.getUsers';
+    const GET_TOKEN_AUTH    = '.getTokenAuth';
+    const SET_USER_ACCESS   = '.setUserAccess';
+    const GET_USERS_LOGIN   = '.getUsersLogin';
+    const GET_USER_BY_EMAIL = '.getUserByEmail';
+    const GET_USERS_WITH_SITE_ACCESS  = '.getUsersWithSiteAccess';
+    const GET_SITES_ACCESS_FROM_USER  = '.getSitesAccessFromUser';
+    const GET_USERS_ACCESS_FROM_SITE  = '.getUsersAccessFromSite';
+    const GET_USERS_SITES_FROM_ACCESS = '.getUsersSitesFromAccess';
+
+    // Visits Summary Method
+    const GET = '.get';
+    const GET_VISITS = '.getVisits';
+    const GET_UNIQUE_VISITORS = '.getUniqueVisitors';
+
+    /**
+     * Piwik API endpoint
+     * @var string
+     */
+    private $piwik_endpoint;
+
+    /**
+     * THis variable will hold the authenticated token upon connection
+     * @var string
+     */
+    private $token_auth;
+
+    /**
+     * This variable will hold the config needed in contructing this Class
+     * @var string|null
+     */
+    public $user_token = null;
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        $this->token_auth = is_null($this->user_token) ? Yii::$app->params['piwikApiKey'] : $this->user_token;
+        $this->piwik_endpoint = Yii::$app->params['piwikUrl'];
+
+        // Initialize parent construct
+        parent::init();
+    }
+
+    /**
+     * Connects to Piwik API and returns a JSON string
+     * @param $method
+     * @param array $params
+     * @param string $format
+     * @return mixed
+     * @throws Exception
+     */
+    protected function request($method, $params = [], $format = 'JSON')
+    {
+        // Default parameters for the API call
+        $default_params = [
+            'token_auth' => $this->token_auth,
+            'module' => 'API',
+            'method' => $method,
+            'format' => $format
+        ];
+
+        // Merge default parameters to the custom parameters passed to $params variable
+        $_params = array_merge($default_params, $params);
+
+        // Initialize Curl Connection
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, $this->piwik_endpoint . '/?' . http_build_query($_params));
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+        $result = curl_exec($ch);
+
+        if ($result === false)
+        {
+            throw new Exception('Curl error: ' . curl_error($ch));
+        }
+        else
+        {
+            return $result;
+        }
+    }
+}
---
+++ PiwikSitesManager.php
@@ -0,0 +1,125 @@
+<?php
+
+
+namespace common\components;
+
+
+class PiwikSitesManager extends PiwikAPI
+{
+    /**
+     * @return mixed
+     */
+    public function getAllSites()
+    {
+        return $this->request(parent::SITES_MANAGER . parent::GET_ALL_SITES);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAllSitesId()
+    {
+        return $this->request(parent::SITES_MANAGER . parent::GET_ALL_SITES_ID);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSitesWithViewAccess()
+    {
+        return $this->request(parent::SITES_MANAGER . parent::GET_SITES_WITH_VIEW_ACCESS);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSitesIdWithViewAccess()
+    {
+        return $this->request(parent::SITES_MANAGER . parent::GET_SITES_ID_WITH_VIEW_ACCESS);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSitesIdWithAtLeastViewAccess()
+    {
+        return $this->request(parent::SITES_MANAGER . parent::GET_SITES_ID_WITH_ATLEAST_VIEW_ACCESS);
+    }
+
+    /**
+     * @param $siteId
+     * @return mixed
+     */
+    public function getSiteFromId($siteId)
+    {
+        $params = [
+            'idSite' => $siteId,
+        ];
+
+        return $this->request(parent::SITES_MANAGER . parent::GET_SITE_FROM_ID, $params);
+    }
+
+    /**
+     * @param $url
+     * @return mixed
+     */
+    public function getSitesIdFromSiteUrl($url)
+    {
+        $params = [
+            'url' => $url,
+        ];
+
+        return $this->request(parent::SITES_MANAGER . parent::GET_SITES_ID_FROM_SITE_URL , $params);
+    }
+
+    /**
+     * @param $siteName
+     * @param $urls
+     * @param array $options
+     * @return mixed
+     */
+    public function addSite($siteName, $urls, $options = [])
+    {
+        $params = [
+            'siteName' => $siteName,
+            'urls'	   => $urls,
+        ];
+
+        $params = array_merge($params, $options);
+
+        return $this->request(parent::SITES_MANAGER . parent::ADD_SITE, $params);
+    }
+
+    /**
+     * @param $siteId
+     * @param $siteName
+     * @param $urls
+     * @param array $options
+     * @return mixed
+     */
+    public function updateSite($siteId, $siteName, $urls, $options = [])
+    {
+        $params = [
+            'idSite'   => $siteId,
+            'siteName' => $siteName,
+            'urls'	   => $urls,
+        ];
+
+        $params = array_merge($params, $options);
+
+        return $this->request(parent::SITES_MANAGER . parent::UPDATE_SITE, $params);
+    }
+
+    /**
+     * @param $siteId
+     * @return mixed
+     */
+    public function deleteSite($siteId)
+    {
+        $params = [
+            'idSite' => $siteId,
+        ];
+
+        return $this->request(parent::SITES_MANAGER . parent::DELETE_SITE, $params);
+    }
+}
